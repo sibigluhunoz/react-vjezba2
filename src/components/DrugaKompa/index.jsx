3fbcc49d38533a34d7253ce3b4ca3294f16b953e
Ana Toplicanec <ana.toplicanec@gmail.com>
2023-03-09 11:56:11 +0100

    dodan podnaslov u drugu komponentu

--- src/components/DrugaKompa/index.jsx
+++ src/components/DrugaKompa/index.jsx
@@ -1,8 +1,13 @@
 const DrugaKompa = () => {
+  const podnaslov = 'JSX';
+  const formatTitle = title => title.toLowerCase();
     return (
+        <>
+        <h2>podnaslov: {formatTitle(podnaslov)}</h2>
         <div>
           <h1>Druga komponenta</h1>
-        </div>  
+          </div>
+        </>
     );
 };
 
